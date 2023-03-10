07bd9fe95f9fe79c213a7689c8475c2de0844e6e
Habacuc Soto <josehabacuc182020@gmail.com>
2023-03-10 10:04:56 -0600

    Change in exercise 10

--- 10/Main.java
+++ 10/Main.java
@@ -2,6 +2,11 @@
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println((5+8+3+2+9) / 5);
+        int[] numeros = {5, 8, 3, 2, 9};
+        int r = 0;
+        for(int i = 0; i < numeros.length; i++){
+            r += numeros[i];
+        }
+        System.out.println(r / numeros.length);
     }
 }
